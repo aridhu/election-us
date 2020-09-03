3c6df40c279633dff6d0d6bbe1ebb1f7b164718b
Bala Sankar <bala@aridhu.com>
2020-09-03 15:14:48 -0700

    removed environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    "apiKey": "AIzaSyCPflWcjG-FpoNFL0JoSFOl3OzkzYK_jCc",
-    "appId": "1:828194985069:web:284b860a155e2373de1b64",
+    "apiKey": "",
+    "appId": "",
     "authDomain": "election-us.firebaseapp.com",
     "databaseURL": "https://election-us.firebaseio.com",
-    "measurementId": "G-7LJ4KFWZ5D",
-    "messagingSenderId": "828194985069",
+    "measurementId": "",
+    "messagingSenderId": "",
     "projectId": "election-us",
     "storageBucket": "election-us.appspot.com"
   }
